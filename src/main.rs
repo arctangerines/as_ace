7286fe415f9375073f02536d73406067cf44c174
arctangerines <149563981+arctangerines@users.noreply.github.com>
2024-01-29 23:03:16 -0500

    Implemented early versions of default, insert and search - Early insert and search with error handling kinda ish

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,44 @@ pub mod ace
     {
         pub fn new() -> Self
         {
-           AceTrie {
-            child: HashMap::new(),
-           } 
+            AceTrie {
+                child: HashMap::new(),
+            }
+        }
+        pub fn default() -> Self { AceTrie::new() }
+        pub fn insert(
+            &mut self,
+            s: String,
+        )
+        {
+            let mut next = self;
+            for c in s.chars()
+            {
+                // TODO: Investigate what next.insert() does
+                next.child.insert(c, AceTrie::default());
+                next = match next.child.get_mut(&c)
+                {
+                    Some(x) => x,
+                    // idk why it wouldnt insert
+                    None => break,
+                }
+            }
+        }
+        pub fn search(
+            &mut self,
+            s: String,
+        )
+        {
+            let mut next = self;
+            for c in s.chars()
+            {
+                match next.child.get_mut(&c)
+                {
+                    Some(x) => next = x,
+                    // This is where we go to start suggesting/correcting
+                    None => break,
+                }
+            }
         }
     }
 }
